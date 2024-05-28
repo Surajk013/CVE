bbb31f5feb4b89744489bc61ce7dd97b89096957
Surajk013 <surajsinghk013@gmail.com>
2024-05-28 18:45:55 +0530

    .

--- procedur.vb
+++ procedur.vb
@@ -1,10 +1,5 @@
 '! available sources
 
-        [ ]study code first
-        [ ]Data collection 
-            NVD -- National Vulnerability Database maintained by NIST and MITRE CVE list   [national institute and standard and technology || Massachusetts Institute of technology Research and Engineering]
-            Mitre
-        [ ]Perfect web visualisation 
-            how frequency and severity has changed over time
-            Trends , Spikes Patterns for -- evolution of cyber threats
-        
+NVD CISA Mitre
+
+https://www.cvedetails.com/vulnerabilities-by-types.php
